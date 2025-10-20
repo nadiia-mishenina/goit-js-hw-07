e0f527245974542674bbbfa18b4fd097d0932013
nadiia-mishenina <ya.nadegda2014@gmail.com>
2025-10-20 20:00:40 +0300

    HW - new

---
+++ js/task-4.js
@@ -0,0 +1,21 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const { email, password } = form.elements;
+
+  const userData = {
+    email: email.value.trim(),
+    password: password.value.trim(),
+  };
+
+  if (userData.email === "" || userData.password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  console.log(userData);
+
+  form.reset();
+});
